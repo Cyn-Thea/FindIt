d844d83f249e4bbecdc41bcf27cc2d753731ccb7
Cyn-Thea <fadzai@net.chalmers.se>
2022-10-08 01:05:29 +0200

    filter posts by catergory

--- client/src/views/Posts.vue
+++ client/src/views/Posts.vue
@@ -1,28 +1,28 @@
 <template>
-    <div>
       <div class="container-md">
-      <h1>Explore</h1>
        <b-button
+          variant="outline-warning"
+          class="btn-style btn-lost"
+          title="Filter by lost"
+          v-on:click="retrieveAllPosts()"
+          >Explore</b-button >
+          <b-button
           variant="outline-info"
           class="btn-style btn-lost"
           title="Filter by lost"
-          v-on:click=";(selectedCategory = 'lost'), sortByCatergory()"
+          v-on:click=";(selectedCategory = 'lost'), sortByCategory()"
           >Lost</b-button >
          <b-button
           variant="outline-success"
           class="btn-style btn-found"
           title="Filter by found"
-          v-on:click=";(selectedCategory = 'found'), sortByCatergory()"
+          v-on:click=";(selectedCategory = 'found'), sortByCategory()"
           >Found</b-button>
       <b-container class="list">
-        <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
-          {{ message }}
-        </b-alert>
-        <b-row id="loading" v-bind:class="isLoading">
-        </b-row>
-        <p v-if="!posts.length && isLoading === 'hideLoading'">There are no posts yet.</p>
-        <b-row v-for="post in posts" v-bind:key="post._id" class="listitem" align-h="center">
-            <b-card>
+        <b-card v-for="post in posts"
+        v-bind:key="post._id"
+        class="listitem"
+        align-h="center">
             <b-col>{{ post.catergory }}</b-col>
             <b-col>Post by: {{ post.author}}</b-col>
             <b-col>Title: {{ post.title }}</b-col>
@@ -32,10 +32,8 @@
              <a :href="'/posts/' + post._id" size="sm">view all comments</a>
           </b-card>
         </b-card>
-        </b-row>
       </b-container>
     </div>
-     </div>
 </template>
 
 <script>
@@ -43,44 +41,31 @@ import { Api } from '@/Api'
 
 export default {
   name: 'posts',
-  props: {
-    user: Object
-  },
-
   mounted() {
-    Api.get('/posts')
-      .then(response => {
-        this.isLoading = 'hideLoading'
-        this.posts = response.data.posts
-      })
-      .catch(error => {
-        this.isLoading = 'hideLoading'
-        if (error.response) {
-          if (error.response.status === 404) {
-            this.message = 'Could not find any posts'
-          }
-        } else {
-          this.message = 'Could not load the Posts, please try again later'
-        }
-        this.showDismissibleAlert = true
-      })
+    this.retrieveAllPosts()
   },
   data() {
     return {
       posts: [],
-      sortedPosts: [],
-      message: '',
       selectedCategory: '',
-      showDismissibleAlert: false,
-      isLoading: '',
       error: ''
     }
   },
   methods: {
-    sortByCatergory() {
+    retrieveAllPosts() {
+      Api.get('/posts')
+        .then((response) => {
+          this.posts = response.data.posts
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    sortByCategory() {
       Api.get('/postFiltered?catergory=' + this.selectedCategory)
         .then((response) => {
-          this.sortedPosts = response.data.sortedPosts
+          this.posts = response.data.posts
           console.log(response)
         })
         .catch((error) => {
