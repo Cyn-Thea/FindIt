f78732d09766f1534f2cec8b3f26664896c0a1c4
Cyn-Thea <fadzai@net.chalmers.se>
2022-10-13 00:55:00 +0200

    delete all users

--- client/src/views/Users.vue
+++ client/src/views/Users.vue
@@ -3,9 +3,12 @@
        <b-button
           id="delete"
           title="All posts"
-          v-on:click="retrieveAllUsers()"
-          >Delete All Users</b-button >
-      <b-container class="listitem"
+          v-on:click="deleteAllUsers()"
+          >Delete All Users</b-button>
+          <div class=" grid">
+             <ul v-if="layout === 'grid'" class="card-list">
+           <p v-if="!users.length && message === ''">There are no users.</p>
+       <b-container class="userlist"
          v-for="user in users"
         v-bind:key="user._id"
         align-h="center">
@@ -14,6 +17,8 @@
             <b-col> Last Name: {{ user.lastName}}</b-col>
             <b-col> Email: {{ user.email}}</b-col>
       </b-container>
+       </ul>
+    </div>
     </div>
 </template>
 
@@ -30,7 +35,9 @@ export default {
   data() {
     return {
       users: [],
-      error: ''
+      error: '',
+      layout: 'grid',
+      message: ''
     }
   },
   methods: {
@@ -44,42 +51,25 @@ export default {
           console.log(error)
         })
     },
-    updateUser() {
-      const updateUser = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        password: this.password
-      }
-      Api.put(`/users/${this.user.id}`, updateUser).then(
-        (res) => {
-          console.log(res)
-          this.message = 'Your Profile has been updated!'
-          this.showDismissibleAlert = true
-        },
-        (err) => {
-          console.log(err.response)
-          this.message = 'Profile could not be updated, please try again later'
-          this.showDismissibleAlert = true
-        }
-      )
-    }
-    /* deleteAllUsers() {
+    deleteAllUsers() {
       Api.delete('/users')
         .then((response) => {
-          this.users = response.data.users
+          localStorage.clear()
           console.log(response)
+          this.$router.push('/', this.$router.go(0))
         })
         .catch((error) => {
           console.log(error)
         })
-    } */
+    }
   }
 }
 </script>
 
 <style>
 #delete {
-  background-color: #e80d0d; /* Green */
+  background-color: #e80d0d;
+  margin-top: 1em;
   border: none;
   color: white;
   padding: 10px 9px;
@@ -87,5 +77,47 @@ export default {
   text-decoration: none;
   display: inline-block;
   font-size: 15px;
+}
+.userlist {
+  margin-bottom: 1em;
+  margin-top: 1.5em;
+  background-color: rgb(125, 176, 188);
+  color: #000000;
+  border: 2px solid #c41693;
+  border-radius: 10px;
+  padding: 2px 6px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+.card-list {
+  display: grid;
+  grid-gap: 2em;
+  grid-template-columns: repeat(3, minmax(100px, 1fr));
+}
+@media only screen and (max-width: 768px) {
+  .container {
+      width: 100%;
+  }
+  .card-list {
+    grid-template-columns: repeat(1, minmax(100px, 1fr));
+  }
+}
+
+@media only screen and (min-width: 1024px) and (max-width: 1200px) {
+  .container {
+      width: 100%;
+  }
+  .card-list {
+    grid-template-columns: repeat(3, minmax(100px, 1fr));
+  }
+}
+@media only screen and (min-width: 768px) and (max-width: 1024px) {
+  .container {
+      width: 100%;
+  }
+  .card-list {
+    grid-template-columns: repeat(2, minmax(100px, 1fr));
+  }
 }
 </style>
