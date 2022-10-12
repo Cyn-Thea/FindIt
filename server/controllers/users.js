71dcea21144243613f9f0de0fc3b5b6ba928fba9
Cyn-Thea <fadzai@net.chalmers.se>
2022-10-12 21:02:02 +0200

    update user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -12,27 +12,14 @@ router.post('/api/users', function(req, res, next){
     })
 });
 
- // create new post (works)
-router.post('/api/users/:id/posts', function(req, res, next) {
-    var id = req.params.id;
-    var post = new Post(req.body);
-    
-    User.findById(id, function(err, user) {
-        if (user == null) {
-            return res.status(404).json({"message": "user not found"});
-        }
+// create new user(works)
+router.post('/api/users', function(req, res, next){
+    var user = new User(req.body);
+    user.save(function(err, user) {
         if (err) { return next(err); }
-
-        post.user = user._id;
-        user.posts.push(post);
-        user.save();
-    });
-     post.save(function(err) {
-        if (err) { return next(err); }
-        res.status(201).json(post);
-    });
+        res.status(201).json(user);
+    })
 });
-
 
 // get all users (works)
 router.get('/api/users', function(req, res, next) {
@@ -51,28 +38,6 @@ router.get('/api/users/:id', function(req, res, next) {
             return res.status(404).json({'Message': 'User not found!'});
         }
         res.json(user);
-    });
-});
-
- // get all posts of a user(works)
- router.get('/api/users/:id/posts', function (req, res, next) {
-    var id = req.params.id;
-   User.findById(id, function (err, user) {
-        if (err) { 
-            return next(err); }
-    }).populate('posts').exec(function (err, user) {
-        if (user == null) { 
-            var err = new Error('No user found');
-            err.status = 404;
-            return next(err); 
-        } 
-        if (user.posts.length == 0) {
-            var err = new Error('No posts found');
-            err.status = 404;
-            return next(err);
-        }
-        console.log('posts with specified post retreived');
-        res.status(200).json(user);
     });
 });
 
@@ -129,7 +94,7 @@ router.delete('/api/users/:id', function(req, res, next) {
 });
 
 //Delete all
-router.delete('/api/users/', function (req, res, next) {
+router.delete('/api/users', function (req, res, next) {
     User.remove({}, function (err, user) {
         if (err) { return next(err); }
         if (user == null) {
