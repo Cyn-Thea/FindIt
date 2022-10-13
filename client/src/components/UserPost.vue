6e39830e87f087c49d93991f4f3a7cf0a1791f80
Cyn-Thea <fadzai@net.chalmers.se>
2022-10-13 23:37:52 +0200

    delete comment

--- client/src/components/UserPost.vue
+++ client/src/components/UserPost.vue
@@ -2,11 +2,12 @@
   <div class="container">
     <div class="row" >
        <b-container class="listitem">
+        <p><b-col>{{ post.category}}</b-col></p>
         <b-col>Title: {{ post.title}}</b-col>
-         <b-col>Description: {{ post.building}}</b-col>
-      <div class="col-12">
-        <p> <b-col> {{ post.description }}</b-col></p>
-      </div>
+         <b-col>Description: {{ post.description}}</b-col>
+        <b-col> Location : {{ post.building }}</b-col>
+        <b-col> Posted On : {{ post.date}}</b-col>
+        <b-button variant="danger" v-on:click="$emit('delete-post', post._id)">Delete</b-button>
       </b-container>
     </div>
   </div>
@@ -19,6 +20,13 @@ export default {
 }
 </script>
 
-<style >
-
+<style>
+p {
+  color: rgb(18, 68, 133);
+  background-color: rgb(125, 184, 190);
+  border: 2px solid #12579b;
+  border-radius: 90px;
+  font-family: courier;
+  font-size: 150%;
+}
 </style>
